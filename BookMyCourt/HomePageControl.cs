4f4948fe823611573e1437bd778e04d1d54e59f5
exustice <obinergy@gmail.com>
2023-06-20 21:07:10 +0800

    forgot password initial phase

---
+++ BookMyCourt/HomePageControl.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookMyCourt
+{
+    public partial class HomePageControl : UserControl
+    {
+
+
+        private string connectionString = @"Data Source=DESKTOP-JJULN80\SQLEXPRESS;Initial Catalog=DBbooking;Integrated Security=True";
+        public HomePageControl()
+        {
+            InitializeComponent();
+            LoadHotels();
+
+        }
+
+
+        private Image ConvertByteArrayToImage(byte[] byteArray)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArray))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+
+
+
+       
+
+        private void LoadHotels()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT HotelName, HotelAddress, HotelStar, HotelOffers, HotelImage FROM Hotels";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string hotelName = reader.GetString(0);
+                            string hotelAddress = reader.GetString(1);
+                            int hotelStar = reader.GetInt32(2);
+                            string hotelOffers = reader.GetString(3);
+                            byte[] hotelImageBytes = (byte[])reader.GetValue(4);
+                            Image hotelImage = ConvertByteArrayToImage(hotelImageBytes);
+
+                            // Create a panel to hold hotel details
+                            Panel hotelPanel = new Panel();
+                            hotelPanel.BorderStyle = BorderStyle.FixedSingle;
+                            hotelPanel.Padding = new Padding(10);
+                            hotelPanel.Size = new Size(745, 180);
+                            hotelPanel.BackColor = Color.White;
+
+                            int maxTextLength = 12;
+                            // Create labels for hotel details
+                            Label lblHotelName = new Label();
+                            lblHotelName.Text = "Hotel Name: " + hotelName;
+                            lblHotelName.AutoSize = true;
+                            lblHotelName.Location = new Point(10, 10);
+                            lblHotelName.Text = "Hotel Name: " + (hotelName.Length <= maxTextLength ? hotelName : hotelName.Substring(0, maxTextLength) + "...");
+                            lblHotelName.Font = new Font(lblHotelName.Font.FontFamily, 15);
+
+                            Label lblHotelAddress = new Label();
+                            lblHotelAddress.Text = "Address: " + hotelAddress;
+                            lblHotelAddress.AutoSize = true;
+                            lblHotelAddress.Location = new Point(10, 50);
+                            lblHotelAddress.Font = new Font(lblHotelAddress.Font.FontFamily, 15);
+                            lblHotelAddress.Text = "Hotel Address: " + (hotelAddress.Length <= maxTextLength ? hotelAddress : hotelAddress.Substring(0, maxTextLength) + "...");
+
+                            Label lblHotelStar = new Label();
+                            lblHotelStar.Text = "Star: " + hotelStar.ToString();
+                            lblHotelStar.AutoSize = true;
+                            lblHotelStar.Location = new Point(10, 90);
+                            lblHotelStar.Font = new Font(lblHotelStar.Font.FontFamily, 15);
+
+                            Label lblHotelOffers = new Label();
+                            lblHotelOffers.Text = "Offers: " + hotelOffers;
+                            lblHotelOffers.AutoSize = true;
+                            lblHotelOffers.Location = new Point(10, 130);
+                            lblHotelOffers.Font = new Font(lblHotelOffers.Font.FontFamily, 15);
+                            lblHotelOffers.Text = "Hotel Offers: " + (hotelOffers.Length <= maxTextLength ? hotelOffers : hotelOffers.Substring(0, maxTextLength) + "...");
+
+                            // Create a PictureBox for hotel image
+                            PictureBox pbHotelImage = new PictureBox();
+                            pbHotelImage.Image = hotelImage;
+                            pbHotelImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                            pbHotelImage.Width = 200;
+                            pbHotelImage.Height = 150;
+                            pbHotelImage.Location = new Point(300, 10);
+
+
+                            // Create a button to check the selected hotel
+                            Button btnCheckHotel = new Button();
+                            btnCheckHotel.Text = "Book Hotel";
+                            btnCheckHotel.Tag = hotelName; // Store the hotel name as the button's tag
+                            btnCheckHotel.Click += btnBooking_Click;
+                            btnCheckHotel.AutoSize = true;
+                            btnCheckHotel.Location = new Point(570, 80);
+                            btnCheckHotel.Font = new Font(lblHotelOffers.Font.FontFamily, 15);
+
+                            // Add labels and picture box to the panel
+                            hotelPanel.Controls.Add(lblHotelName);
+                            hotelPanel.Controls.Add(lblHotelAddress);
+                            hotelPanel.Controls.Add(lblHotelStar);
+                            hotelPanel.Controls.Add(lblHotelOffers);
+                            hotelPanel.Controls.Add(pbHotelImage);
+                            hotelPanel.Controls.Add(btnCheckHotel);
+
+                            // Add the panel to the FlowLayoutPanel
+                            flowLayoutPanelHotels.Controls.Add(hotelPanel);
+                        }
+                    }
+                }
+            }
+        }
+        private void btnBooking_Click(object sender, EventArgs e)
+        {
+            Button btnCheckHotel = (Button)sender; // Get the clicked button
+            string hotelName = btnCheckHotel.Tag.ToString(); // Retrieve the hotel name from the button's tag
+
+            // Close the HomePageControl
+            this.Hide();
+
+            // Create an instance of the BookingPageControl and pass the hotelName
+            BookingPageControl bookingPageControl = new BookingPageControl();
+            bookingPageControl.SetHotelName(hotelName);
+
+            // Add the bookingPageControl to the controls of the main form
+            Parent.Controls.Add(bookingPageControl);
+
+            // Set the docking style of the bookingPageControl to fill the entire form
+            bookingPageControl.Dock = DockStyle.Fill;
+
+            // Show the bookingPageControl
+            bookingPageControl.Show();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchText = txtSearch.Text.Trim();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT HotelName, HotelAddress, HotelStar, HotelOffers, HotelImage FROM Hotels WHERE HotelName LIKE @SearchText OR HotelAddress LIKE @SearchText OR HotelOffers LIKE @SearchText";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@SearchText", "%" + searchText + "%");
+
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Clear existing hotel panels
+                        flowLayoutPanelHotels.Controls.Clear();
+
+                        while (reader.Read())
+                        {
+                            string hotelName = reader.GetString(0);
+                            string hotelAddress = reader.GetString(1);
+                            int hotelStar = reader.GetInt32(2);
+                            string hotelOffers = reader.GetString(3);
+                            byte[] hotelImageBytes = (byte[])reader.GetValue(4);
+                            Image hotelImage = ConvertByteArrayToImage(hotelImageBytes);
+
+                            // Create a panel to hold hotel details
+                            Panel hotelPanel = new Panel();
+                            hotelPanel.BorderStyle = BorderStyle.FixedSingle;
+                            hotelPanel.Padding = new Padding(10);
+                            hotelPanel.Size = new Size(745, 180);
+                            hotelPanel.BackColor = Color.White;
+
+                            int maxTextLength = 12;
+                            // Create labels for hotel details
+                            Label lblHotelName = new Label();
+                            lblHotelName.Text = "Hotel Name: " + hotelName;
+                            lblHotelName.AutoSize = true;
+                            lblHotelName.Location = new Point(10, 10);
+                            lblHotelName.Text = "Hotel Name: " + (hotelName.Length <= maxTextLength ? hotelName : hotelName.Substring(0, maxTextLength) + "...");
+                            lblHotelName.Font = new Font(lblHotelName.Font.FontFamily, 15);
+
+                            Label lblHotelAddress = new Label();
+                            lblHotelAddress.Text = "Address: " + hotelAddress;
+                            lblHotelAddress.AutoSize = true;
+                            lblHotelAddress.Location = new Point(10, 50);
+                            lblHotelAddress.Font = new Font(lblHotelAddress.Font.FontFamily, 15);
+                            lblHotelAddress.Text = "Hotel Address: " + (hotelAddress.Length <= maxTextLength ? hotelAddress : hotelAddress.Substring(0, maxTextLength) + "...");
+
+                            Label lblHotelStar = new Label();
+                            lblHotelStar.Text = "Star: " + hotelStar.ToString();
+                            lblHotelStar.AutoSize = true;
+                            lblHotelStar.Location = new Point(10, 90);
+                            lblHotelStar.Font = new Font(lblHotelStar.Font.FontFamily, 15);
+
+                            Label lblHotelOffers = new Label();
+                            lblHotelOffers.Text = "Offers: " + hotelOffers;
+                            lblHotelOffers.AutoSize = true;
+                            lblHotelOffers.Location = new Point(10, 130);
+                            lblHotelOffers.Font = new Font(lblHotelOffers.Font.FontFamily, 15);
+                            lblHotelOffers.Text = "Hotel Offers: " + (hotelOffers.Length <= maxTextLength ? hotelOffers : hotelOffers.Substring(0, maxTextLength) + "...");
+
+                            // Create a PictureBox for hotel image
+                            PictureBox pbHotelImage = new PictureBox();
+                            pbHotelImage.Image = hotelImage;
+                            pbHotelImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                            pbHotelImage.Width = 200;
+                            pbHotelImage.Height = 150;
+                            pbHotelImage.Location = new Point(300, 10);
+
+                            // Create a button to check the selected hotel
+                            Button btnCheckHotel = new Button();
+                            btnCheckHotel.Text = "Book Hotel";
+                            btnCheckHotel.Tag = hotelName; // Store the hotel name as the button's tag
+                            btnCheckHotel.Click += btnBooking_Click;
+                            btnCheckHotel.AutoSize = true;
+                            btnCheckHotel.Location = new Point(570, 80);
+                            btnCheckHotel.Font = new Font(lblHotelOffers.Font.FontFamily, 15);
+
+                            // Add labels and picture box to the panel
+                            hotelPanel.Controls.Add(lblHotelName);
+                            hotelPanel.Controls.Add(lblHotelAddress);
+                            hotelPanel.Controls.Add(lblHotelStar);
+                            hotelPanel.Controls.Add(lblHotelOffers);
+                            hotelPanel.Controls.Add(pbHotelImage);
+                            hotelPanel.Controls.Add(btnCheckHotel);
+
+                            // Add the panel to the FlowLayoutPanel
+                            flowLayoutPanelHotels.Controls.Add(hotelPanel);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    }
---
+++ BookMyCourt/LoginForm.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace BookMyCourt
+{
+    public partial class LoginForm : Form
+    {
+        private RegistrationForm registrationForm;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text;
+
+            bool isValidEmail = Regex.IsMatch(email, @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$");
+            bool containsSpaces = email.Contains(" ");
+            // Check if email and password are not empty
+            if (!isValidEmail || containsSpaces)
+            {
+                MessageBox.Show(txtEmail, "Invalid email address");
+
+            }
+
+           else if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+            {
+                // Connect to the database
+                string connectionString = @"Data Source=DESKTOP-JJULN80\SQLEXPRESS;Initial Catalog=DBbooking;Integrated Security=true;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Check if the user exists in the database
+                    string query = "SELECT COUNT(*), IsAdmin FROM Users WHERE email = @Email AND password = @Password GROUP BY IsAdmin";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            int count = reader.GetInt32(0);
+                            bool isAdmin = reader.GetBoolean(1);
+
+                            if (count > 0)
+                            {
+                                if (isAdmin)
+                                {
+                                    // User is an admin, open AdminDashboardForm
+                                    MessageBox.Show("Login successful as admin.");
+                                    this.Hide(); // Hide the login form
+                                    AdminDashboardForm adminDashboard = new AdminDashboardForm();
+                                    adminDashboard.Show();
+                                }
+                                else
+                                {
+                                    // User is not an admin, open DashboardForm
+                                    MessageBox.Show("Login successful.");
+                                    this.Hide(); // Hide the login form
+                                    DashboardForm dashboard = new DashboardForm();
+                                    dashboard.Show();
+                                }
+                            }
+
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please try again!");
+                        }
+                    }
+                }
+
+
+            }
+         
+
+            else
+            {
+                MessageBox.Show("Please Enter your email and password.");
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // Navigate to the registration form
+            RegistrationForm regForm = new RegistrationForm();
+            this.Hide();
+            regForm.Show();
+        }
+
+        private void forgotPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (registrationForm == null)
+            {
+                MessageBox.Show("Registration form is not available.");
+                return;
+            }
+
+            // Access the registrationForm's properties
+            string secretQuestion = registrationForm.SelectedSecretQuestion;
+            string secretAnswer = registrationForm.SecretAnswer;
+
+            string email = txtEmail.Text;
+
+
+            string connectionString = @"Data Source=DESKTOP-JJULN80\SQLEXPRESS;Initial Catalog=DBbooking;Integrated Security=true;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Check if the email exists in the database
+                string query = "SELECT secretQuestion, secretAnswer FROM Users WHERE email = @Email";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Email", email);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string storedSecretQuestion = reader["secretQuestion"].ToString();
+                    string storedSecretAnswer = reader["secretAnswer"].ToString();
+
+                    if (secretQuestion.Equals(storedSecretQuestion) && secretAnswer.Equals(storedSecretAnswer))
+                    {
+                        // Secret question and answer are correct, allow password reset
+                        string newPassword = GenerateRandomPassword(); // Replace this with your password reset logic
+
+                        // Update the password in the database
+                        query = "UPDATE Users SET password = @NewPassword WHERE email = @Email";
+                        command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@NewPassword", newPassword);
+                        command.Parameters.AddWithValue("@Email", email);
+
+                        int result = command.ExecuteNonQuery();
+
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Password reset successful. Your new password is: " + newPassword);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Password reset failed. Please try again.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect secret question or answer. Please try again.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Email not found. Please enter a valid email address.");
+                }
+
+                reader.Close();
+            }
+        }
+
+        private string GenerateRandomPassword()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var newPassword = new string(
+                Enumerable.Repeat(chars, 10)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+
+            return newPassword;
+        }
+
+        // Event handler for the "Forgot Password" link label
+        private void lnkForgotPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
